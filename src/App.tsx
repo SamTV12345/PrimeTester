e1f385e1c1fcccd7f61d6eb0dcdb79638abe3816
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2022-12-19 19:29:44 +0100

    Added validation.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,31 @@ import {CheckIcon} from "@/CheckIcon";
 import {CrossIcon} from "@/CrossIcon";
 import {carMichaelNumbers} from "@/SpecialNumbers";
 import {BigMath} from "@/BigIntUtils";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+
+type Inputs = {
+  testableNumber: bigint,
+  round: number,
+  carmichael: boolean
+};
 
 const App: React.FC = () => {
-  const [enteredNumber, setEnteredNumber] = useState(5n)
-  const [numberRounds, setNumberRounds] = useState(2)
-  const [detectCarMichael, setDetectCarMichael] = useState<boolean>(true)
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  let enteredNumber = 0n
+  let numberRounds = 0
+  let detectCarMichael = false
   const [isPrim, setIsPrim] = useState<boolean>(false)
   const [progress, setProgress] = useState<number>(0)
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    numberRounds = data.round
+    enteredNumber = BigInt(data.testableNumber)
+    detectCarMichael = data.carmichael
+    checkIfPrim()
+  }
+
+
 
   const calcGCD = (a: bigint, b: bigint) => {
     a = BigMath.abs(a);
@@ -99,44 +117,31 @@ const App: React.FC = () => {
     console.log("Done")
   }
 
-  return <div className="flex h-screen bg-white dark:bg-slate-800">
+
+  return <form className="flex h-screen bg-white dark:bg-slate-800" onSubmit={handleSubmit(onSubmit)}>
     <div className="m-auto p-6">
       <h1 className="text-4xl dark:text-white mb-4 text-center">Primzahl-Tester</h1>
       <div className="grid grid-cols-2 gap-4">
         <label htmlFor="test-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zu testende
           Zahl</label>
-        <input value={enteredNumber.toString()} onChange={(e) =>{
-          if(Number(e.target.value)<=0){
-            return
-          }
-          setEnteredNumber(BigInt(e.target.value))
-        }}
+        <input {...register("testableNumber", {min: 2, required: true})}
                id="test-input"
                className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        {errors.testableNumber && <div className="col-span-2 text-red-500 text-sm text-center">Die zu testende Zahl muss mindestens 2 sein</div>}
+
         <label htmlFor="number-rounds" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Anzahl
           Runden</label>
-        <input type="number" id="number-rounds" value={numberRounds}
-               onChange={(e) => {
-                 const num = Number(e.target.value)
-                 if(num<=0 || num>=enteredNumber){
-                   return
-                 }
-                 setNumberRounds(Number(e.target.value))
-               }}
+        <input type="number" id="number-rounds" {...register("round", {min: 1, required:true})}
                className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        {errors.round && <div className="col-span-2 text-red-500 text-sm text-center">Die zu testende Zahl muss mindestens 1 sein</div>}
       </div>
 
       <div className="flex gap-4 dark:text-white">
         <label htmlFor="carmichael">Carmichael-Zahlen entdecken?</label>
-        <input id="carmichael" type="checkbox" checked={detectCarMichael}
-               onChange={() => setDetectCarMichael(!detectCarMichael)}/>
+        <input id="carmichael" type="checkbox" {...register("carmichael")}/>
       </div>
       <div className="flex justify-center mt-4">
-        <button className="text-white bg-blue-800 p-2 rounded w-3/4" onClick={async () => {
-          await checkIfPrim()
-        }
-        }>Vermuten
-        </button>
+        <input className="text-white bg-blue-800 p-2 rounded w-3/4" type="submit" value="Vermuten"/>
       </div>
       <div className="flex justify-center mt-2">
         <div className="">
@@ -151,7 +156,7 @@ const App: React.FC = () => {
         <div className="bg-blue-600 h-2.5 rounded-full" style={{width:progress+'%'}}></div>
       </div>
     </div>
-  </div>
+  </form>
 }
 
 
